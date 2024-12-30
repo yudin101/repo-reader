1f69bb5624846e6d7fd56d355f1ba928e7609bce
Yudin Karki <yudinkarki00@gmail.com>
2024-12-30 19:00:33 +0545

    prettier

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,65 +1,74 @@
-import { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import ReactMarkdown from 'react-markdown'
-import DOMPurify from 'dompurify'
-import styles from './Post.module.css'
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactMarkdown from "react-markdown";
+import DOMPurify from "dompurify";
+import styles from "./Post.module.css";
 
 export default function Post({ data, owner, repo }) {
-  const { path } = useParams()
+	const { path } = useParams();
 
-  const item = data.find(post => {
-    return post.path === String(path);
-  });
+	const item = data.find((post) => {
+		return post.path === String(path);
+	});
 
-  const [rawContent, setRawContent] = useState(null)
-  const [rawContentType, setRawContentType] = useState(null)
+	const [rawContent, setRawContent] = useState(null);
+	const [rawContentType, setRawContentType] = useState(null);
 
-  useEffect(() => {
-    async function fetchRawContent(owner, repo, fileName) {
-      const response = await fetch(`https://raw.githubusercontent.com/${owner}/${repo}/refs/heads/main/${fileName}`)
-      const data = await response.text()
+	useEffect(() => {
+		async function fetchRawContent(owner, repo, fileName) {
+			const response = await fetch(
+				`https://raw.githubusercontent.com/${owner}/${repo}/refs/heads/main/${fileName}`,
+			);
+			const data = await response.text();
 
-      const extension = fileName.split('.').pop()
-      setRawContentType(extension)
+			const extension = fileName.split(".").pop();
+			setRawContentType(extension);
 
-      if (extension === 'md') {
-        setRawContent(data)
-      } else {
-        const paragraphs = data.split('\n').map(paragraph => paragraph.trim())
-        setRawContent(paragraphs)
-      }
-    }
+			if (extension === "md") {
+				setRawContent(data);
+			} else {
+				const paragraphs = data
+					.split("\n")
+					.map((paragraph) => paragraph.trim());
+				setRawContent(paragraphs);
+			}
+		}
 
-    fetchRawContent(owner, repo, item.name)
-  }, [])
+		fetchRawContent(owner, repo, item.name);
+	}, []);
 
-
-  if (!item || !rawContent) {
-    return (
+	if (!item || !rawContent) {
+		return (
 			<div className={styles.postBody}>
 				<p>Loading...</p>
 			</div>
-		)
-  }	
+		);
+	}
 
-  return (
-    <>
-      <div className={styles.postBody}>
-        <Link className={styles.goBackButton} to="/repo-reader/">Back</Link>
-        <h2>{item.name}</h2>
-        <h5 className={styles.postMetadata}>{owner}</h5>
+	return (
+		<>
+			<div className={styles.postBody}>
+				<Link className={styles.goBackButton} to="/repo-reader/">
+					Back
+				</Link>
+				<h2>{item.name}</h2>
+				<h5 className={styles.postMetadata}>{owner}</h5>
 
-        {rawContentType !== 'md' && rawContent.map((paragraph, index) => (
-          <p key={index}>{paragraph}<br /></p>
-        ))}
+				{rawContentType !== "md" &&
+					rawContent.map((paragraph, index) => (
+						<p key={index}>
+							{paragraph}
+							<br />
+						</p>
+					))}
 
-        {rawContentType === 'md' && 
-					<ReactMarkdown 
-					  children={DOMPurify.sanitize(rawContent)}
-					  skipHtml={true}
+				{rawContentType === "md" && (
+					<ReactMarkdown
+						children={DOMPurify.sanitize(rawContent)}
+						skipHtml={true}
 					/>
-        }
-      </div>
-    </>
-  )
+				)}
+			</div>
+		</>
+	);
 }
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,41 +1,48 @@
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'
-import styles from './PostList.module.css'
-
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import styles from "./PostList.module.css";
 
 export default function PostList({ data, owner, repo }) {
-  const [rawData, setRawData] = useState([])
+	const [rawData, setRawData] = useState([]);
 
-  async function fetchRawData(owner, repo, fileName) {
-    const response = await fetch(`https://raw.githubusercontent.com/${owner}/${repo}/refs/heads/main/${fileName}`)
-    return await response.text()
-  }
+	async function fetchRawData(owner, repo, fileName) {
+		const response = await fetch(
+			`https://raw.githubusercontent.com/${owner}/${repo}/refs/heads/main/${fileName}`,
+		);
+		return await response.text();
+	}
 
-  useEffect(() => {
-  	async function fetchAllData() {
-    	const content = await Promise.all(
-        data.map((item) => fetchRawData(owner, repo, item.name))
-      );
-      setRawData(content);
-    }
+	useEffect(() => {
+		async function fetchAllData() {
+			const content = await Promise.all(
+				data.map((item) => fetchRawData(owner, repo, item.name)),
+			);
+			setRawData(content);
+		}
 
-    if (data.length > 0) {
-    	fetchAllData();
-    }
-  }, [data, owner, repo]);
+		if (data.length > 0) {
+			fetchAllData();
+		}
+	}, [data, owner, repo]);
 
-  return (
-    <>
-      {data.map((item, index) => (
-        <Link key={index} className={styles.postListItem} to={`/repo-reader/${item.path}`}>
-          <div className={styles.postListTextContainer}>
-            <h2 className={styles.postListTitle}>{item.name}</h2>
-            <p className={styles.postListContent}>
-              {rawData[index] !== undefined ? `${rawData[index].slice(0, 350)}...` : "Loading..."}
-            </p>
-          </div>
-        </Link>
-      ))}
-    </>
-  )
+	return (
+		<>
+			{data.map((item, index) => (
+				<Link
+					key={index}
+					className={styles.postListItem}
+					to={`/repo-reader/${item.path}`}
+				>
+					<div className={styles.postListTextContainer}>
+						<h2 className={styles.postListTitle}>{item.name}</h2>
+						<p className={styles.postListContent}>
+							{rawData[index] !== undefined
+								? `${rawData[index].slice(0, 350)}...`
+								: "Loading..."}
+						</p>
+					</div>
+				</Link>
+			))}
+		</>
+	);
 }
